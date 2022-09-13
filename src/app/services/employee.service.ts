c8dedd49bd4943d70278feee58c5cd0e5ae0c590
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-09-13 15:50:48 -0400

    Create employee interface and service

---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private apiServiceUri = '';
+
+  constructor(private http: HttpClient) {}
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.apiServiceUri + '/employee/all');
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(
+      this.apiServiceUri + '/employee/add',
+      employee
+    );
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(
+      this.apiServiceUri + '/employee/update',
+      employee
+    );
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(
+      this.apiServiceUri + '/employee/delete/' + employeeId
+    );
+  }
+}
