aac7e03bd596005cb65d0d96c7af42fd2e88ea91
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-09-13 17:46:11 -0400

    Add bootstrap html and css to display data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './models/employee';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'employeeManagerAppUI';
+export class AppComponent implements OnInit {
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (res) => (this.employees = res),
+      error: (err) => alert(err.message),
+    });
+  }
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Employee } from '../models/employee';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  private apiServiceUri = '';
+  private apiServiceUri = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) {}
 
