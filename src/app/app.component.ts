960c972cac300f1823442a107a540c5bd9d4e7a6
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-09-15 22:23:02 -0400

    Implement form for adding new employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './models/employee';
 import { EmployeeService } from './services/employee.service';
 
@@ -22,5 +23,38 @@ export class AppComponent implements OnInit {
       next: (res) => (this.employees = res),
       error: (err) => alert(err.message),
     });
+  }
+
+  public onAddEmployee(addForm: NgForm): void {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.getEmployees();
+      },
+      error: (err) => {
+        alert(err.message);
+      },
+    });
+  }
+
+  public onOpenModal(employee: any, mode: String): void {
+    console.log('clicked');
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-bs-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-bs-target', '#addEmployeeModal');
+    }
+    if (mode === 'update') {
+      button.setAttribute('data-bs-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-bs-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
   }
 }
